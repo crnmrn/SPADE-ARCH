570a3a20a129f1e5784d26d38dab585ff8c5bab1
Curtis Mackay <4curtismackay@gmail.com>
2015-09-10 22:40:38 -0700

    Added new tests and finished writing the dependency tracker.

--- compiler/dpda_compiler/parser/dependency_tracker.py
+++ compiler/dpda_compiler/parser/dependency_tracker.py
@@ -1,16 +1,29 @@
-__author__ = 'Curtis'
+__author__ = 'Curtis Mackay'
 
 pe_index = 0;
-
+import itertools
 
 class PE(object):
     """
     Data structure for storing PE dependency data
     """
-    def __init__(self, operation, pe_inputs, pe_outputs):
+    newid = itertools.count().__next__
+
+    def __init__(self, operation, pe_inputs, pe_outputs, pe_id=None):
         self.operation = operation
         self.pe_inputs = pe_inputs
         self.pe_outputs = pe_outputs
+        if pe_id is None:
+            self.pe_id = PE.newid()
+        else:
+            self.pe_id = pe_id
+
+    def __repr__(self):
+        return "PE %s" % self.pe_id
+        # return "PE inputs:%s, outputs:%s" % (self.pe_inputs, self.pe_outputs)
+
+    def __str__(self):
+        return "PE inputs:%s, outputs:%s" % (self.pe_inputs, self.pe_outputs)
 
 
 def compress_mapping(pe_list, net_map):
@@ -25,30 +38,24 @@ def compress_mapping(pe_list, net_map):
                     pe.pe_outputs[i] = net_set[0]
 
 
-def create_dependency_list(pe_list, net_map):
+def create_dependency_list(pe_list):
     dep_list = list()
     for pe in pe_list:
         for pe_dep_candidate in pe_list:
-            if any(pe_io in pe.pe_inputs for pe_io in pe_dep_candidate.outputs):
+            input_set = set(pe.pe_inputs)
+            output_set = set(pe_dep_candidate.pe_outputs)
+            if input_set & output_set:
+                new_dependency = True
                 for dep in dep_list:
                     if dep[-1] is pe_dep_candidate:
                         dep.append(pe)
-                dep_list.append([pe_dep_candidate, pe])
-
-
-
-
-
-    for pe_x in pe_list:
-        for pe_y in pe_list:
-            if pe_x is not pe_y:
-                """Check if pe_x.input map to pe_y.output and vica versa"""
-                for nets in net_map:
-                    for pe_in in pe_x.input:
-                        if pe_in in nets and pe_y.output in nets:
-                            for dep_chain in dep_list:
-                                if pe_y in dep_chain[0]:
-                                    dep_chain.append(pe_x)
-                                else:
-                                    dep_list.append([pe_y, pe_x])
-    return list()
+                        new_dependency = False
+                if new_dependency:
+                    dep_list.append([pe_dep_candidate, pe])
+    # Add in any PE's that have no dependency
+    for pe in pe_list:
+        # optimized way to flatten list
+        flattened_dep_list = [item for sublist in dep_list for item in sublist]
+        if pe not in set(flattened_dep_list):
+            dep_list.append([pe])
+    return dep_list
--- compiler/dpda_compiler/parser/unit_tests/dependency_tracker_test.py
+++ compiler/dpda_compiler/parser/unit_tests/dependency_tracker_test.py
@@ -3,27 +3,33 @@ __author__ = 'Curtis'
 import unittest
 import dpda_compiler.parser.dependency_tracker as dep
 
-pe_list = [dep.PE('mult', ['a', 'b'], ['u']),
-           dep.PE('mult', ['c', 'd'], ['v']),
-           dep.PE('mult', ['e', 'f'], ['w']),
-           dep.PE('mult', ['g', 'h'], ['x']),
-           dep.PE('mult', ['i', 'j'], ['y']),
-           dep.PE('mult', ['k', 'l'], ['z'])]
+pe_list = [dep.PE('mult', ['a', 'b'], ['u'], 0),
+           dep.PE('mult', ['c', 'd'], ['v'], 1),
+           dep.PE('mult', ['e', 'f'], ['w'], 2),
+           dep.PE('mult', ['g', 'h'], ['x'], 3),
+           dep.PE('mult', ['i', 'j'], ['y'], 4),
+           dep.PE('mult', ['k', 'l'], ['z'], 5),
+           dep.PE('mult', ['q', 'r'], ['o'], 6),
+           dep.PE('mult', ['1', '2'], ['3'], 7)]
 
 net_map = [('u', 'm', 'e'), ('v', 'k'), ('w', 'h'), ('x', 'n', 'i'), ('o', 'f')]
 
+dep_list_expect = [[pe_list[0], pe_list[2], pe_list[3], pe_list[4]],
+                   [pe_list[1], pe_list[5]],
+                   [pe_list[6], pe_list[2], pe_list[3], pe_list[4]],
+                   [pe_list[7]]]
 
 class DependencyTests(unittest.TestCase):
     def test_make_dependency_list(self):
-        dep_list = dep.create_dependency_list(pe_list, net_map)
+        dep_list = dep.create_dependency_list(pe_list)
         self.assertIsInstance(dep_list, list)
 
     def test_check_valid_dependency_list(self):
-        dep_list = dep.create_dependency_list(pe_list, net_map)
+        dep.compress_mapping(pe_list, net_map)
+        dep_list = dep.create_dependency_list(pe_list)
         self.assertGreater(len(dep_list), 0)
-        for pe_dep in dep_list:
-            self.assertIsInstance(pe_dep, list)
-            self.assertGreaterEqual(len(pe_dep), 1)
+        for dep_item in dep_list_expect:
+            self.assertIn(dep_item, dep_list, "%s not in dependency list" % dep_item)
 
     def test_compress_mapping(self):
         dep.compress_mapping(pe_list, net_map)
